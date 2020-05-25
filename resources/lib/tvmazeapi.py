efd86c06a3d28ae7554504211a413c489dcd82eb
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-05-25 12:06:59 -1000

    simplied calling structure, added some additional calls

--- resources/lib/tvmazeapi.py
+++ resources/lib/tvmazeapi.py
@@ -2,33 +2,54 @@
 
 from resources.lib.url import URL
 JSONURL = URL( 'json' )
+TXTURL = URL()
 
 
 class TVMaze( object ):
 
     def __init__( self, user='', apikey='' ):
-        self.URL = 'https://api.tvmaze.com'
+        self.PUBLICURL = 'https://api.tvmaze.com'
         if user and apikey:
-            self.AUTHURL = 'https://%s:%s@api.tvmaze.com' % (user, apikey)
+            self.AUTHURL = 'https://%s:%s@api.tvmaze.com/v1/user' % (user, apikey)
         else:
-            self.AUTHURL = self.URL
+            self.AUTHURL = self.PUBLICURL
 
 
     def getShow( self, tvmazeid, params=None ):
-        params = self._convert_params( params )
-        url = '%s/shows/%s' % (self.URL, tvmazeid)
-        return JSONURL.Get( url, params=params )
+        return self._call( 'shows/%s' % tvmazeid, params )
 
 
     def getEpisode( self, episodeid, params=None ):
-        params = self._convert_params( params )
-        url = '%s/episodes/%s' % (self.URL, episodeid)
-        return JSONURL.Get( url, params=params )
-        
+        return self._call( 'episodes/%s' % episodeid, params )
 
-    def _convert_params( self, params ):
+
+    def getFollowedShows( self, params=None ):
+        return self._call( 'follows/shows', params, auth=True )
+
+
+    def getTaggedShows( self, tag, params=None ):
+        return self._call( 'tags/%s/shows' % tag, params, auth=True )
+
+
+    def unTagShow( self, show, tag, params=None ):
+        return self._call( 'tags/%s/shows/%s' % (tag, show), params, auth=True, type='delete' )
+
+
+    def _call( self, url_end, params, auth=False, type="get" ):
+        loglines = []
         if not params:
-            return {}
+            params = {}
+        if auth:
+            if self.AUTHURL == self.PUBLICURL:
+                loglines.append( 'authorization credentials required but not supplied' )
+                return False, loglines, {}
+            url_base = self.AUTHURL
         else:
-            return params
-            
+            url_base = self.PUBLICURL
+        url = '%s/%s' % (url_base, url_end )
+        if type == 'get':
+            success, j_loglines, results = JSONURL.Get( url, params=params )
+        if type == 'delete':
+            success, j_loglines, results = TXTURL.Delete( url, params=params )
+        loglines = loglines + j_loglines
+        return success, loglines, results
