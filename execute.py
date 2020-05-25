8bd6e972b957116eef9638dcb1a0ab88ecdc9666
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-05-24 20:00:59 -1000

    initial upload

---
+++ execute.py
@@ -0,0 +1,9 @@
+# *  Credits:
+# *
+# *  v.0.1.0
+# *  original Schedule Recordings code by pkscout
+
+from resources.lib.schedule import Main
+
+if ( __name__ == "__main__" ):
+    Main()
---
+++ resources/config.py
@@ -0,0 +1,34 @@
+defaults = { 'dvr_type': 'nextpvr',
+             'dvr_host': 'localhost',
+             'dvr_port': '8866',
+             'dvr_user': '',
+             'dvr_auth': '',
+             'dvr_params': { 'recurring_type':1 },
+             'tvmaze_user': '',
+             'tvmaze_apikey': '',
+             'lookforward': 10,
+             'dateformat': '%Y-%m-%d',
+             'aborttime': 30,
+             'logbackups': 7,
+             'debug': False }
+
+try:
+    import data.settings as overrides
+    has_overrides = True
+except ImportError:
+    has_overrides = False
+
+
+def Reload():
+    if has_overrides:
+        reload( overrides )
+
+
+def Get( name ):
+    setting = None
+    if has_overrides:
+        setting = getattr(overrides, name, None)
+    if not setting:
+        setting = defaults.get( name, None )
+    return setting
+    
