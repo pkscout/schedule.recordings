9a52fd0ad059e79ee48b2668103961b2e621c94f
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-05-26 11:03:28 -1000

    fix for better import * of dvr directory

--- resources/lib/dvrs/__init__.py
+++ resources/lib/dvrs/__init__.py
@@ -1,1 +1,6 @@
-__all__ = ['nextpvr']
+import os
+
+__all__ = []
+for item in os.listdir( os.path.dirname( os.path.abspath( __file__ ) ) ):
+    if item.endswith( '.py' ) and item != '__init__.py':
+      __all__.append( item[:-3] )
